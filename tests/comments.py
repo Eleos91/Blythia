71e72698e9479d9cd3ad091748414f9db0a2719a
Joshua Sole <joshua.sole@gmx.de>
2025-01-30 17:42:18 +0100

    Add test for in line comments

---
+++ tests/comments.py
@@ -0,0 +1,7 @@
+var x: u64 = 2 # this is a comment
+var y: f64 = 2.0 # this is another comment
+print_int(x)
+
+def test() -> void: # comment before indention
+  print_int(420) # comment befor removing indention
+print_int(421)
